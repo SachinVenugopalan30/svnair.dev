bb93b58539b8b9816521b0f27e28e27e93e2776b
SachinVenugopalan30 <sachin.venugopal795@gmail.com>
2026-02-10 18:33:53 -0700

    update layout.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Serif, Outfit, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const ibmPlexSerif = IBM_Plex_Serif({
@@ -44,18 +45,20 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${ibmPlexSerif.variable} ${outfit.variable} ${jetbrainsMono.variable}`}
+      suppressHydrationWarning
     >
-      <head>
+      <head />
+      <body>
+        {children}
         {process.env.NEXT_PUBLIC_UMAMI_API_URL &&
-          process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
-            <script
-              defer
-              src={`${process.env.NEXT_PUBLIC_UMAMI_API_URL}/script.js`}
-              data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-            />
-          )}
-      </head>
-      <body>{children}</body>
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID ? (
+          <Script
+            src={`${process.env.NEXT_PUBLIC_UMAMI_API_URL}/script.js`}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="afterInteractive"
+          />
+        ) : null}
+      </body>
     </html>
   );
 }
